8fe0f09b85fec961bc2fba780fbd81a49e015e02
Ishan <ishan.g@softcodeit.com>
2018-04-26 13:27:52 +1000

    * Initial commit

---
+++ IosTest/IosTest/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IosTest
+//
+//  Created by Ishan on 4/26/18.
+//  Copyright © 2018 Ishan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IosTest/IosTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IosTest
+//
+//  Created by Ishan on 4/26/18.
+//  Copyright © 2018 Ishan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
